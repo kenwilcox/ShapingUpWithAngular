fa7cfefc23a76c4870028073f1744bc7259e7381
Ken Wilcox <wilcoxk@gmail.com>
2015-02-24 00:31:40 -0700

    added images with some bogus pictures

--- app.js
+++ app.js
@@ -9,14 +9,31 @@
     name: 'Dodecahedron',
     price: 2.95,
     description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
-    canPurchase: false,
-    soldOut: false,
+    images: [
+      {
+        full: 'http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-02.gif',
+        thumb: '',
+      },
+      {
+        full: '',
+        thumb: '',
+      }
+    ],
   },
   {
     name: 'Pentagonal Gem',
     price: 5.95,
     description: 'Origin of the Pentagonal Gem is unknown, hence its low value. It has a very high shine and 12 sides.',
-    canPurchase: true,
+    images: [
+      {
+        full: 'http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-01.gif',
+        thumb: '',
+      },
+      {
+        full: '',
+        thumb: '',
+      }
+    ],
   }
 ];
 })();
