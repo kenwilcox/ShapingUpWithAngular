27809041ce257d24aced48a08b0c2a4df84fe8bd
Ken Wilcox <wilcoxk@gmail.com>
2015-02-23 22:25:23 -0700

    changed a single gem to an array of gems

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 (function() {
   var app = angular.module('store', [ ] );
   app.controller('StoreController', function () {
-    this.product = gem;
+    this.products = gems;
   });
 
-  var gem = {
+  var gems = [
+  {
     name: 'Dodecahedron',
     price: 2.95,
     description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
-  };
+    canPurchase: false,
+    soldOut: false,
+  },
+  {
+    name: 'Pentagonal Gem',
+    price: 5.95,
+    description: 'Origin of the Pentagonal Gem is unknown, hence its low value. It has a very high shine and 12 sides.',
+    canPurchase: true,
+  }
+];
 })();
