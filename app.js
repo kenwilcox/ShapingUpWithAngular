ee57ce3e20d456ba99325cb8ab9ac5272a167330
Ken Wilcox <wilcoxk@gmail.com>
2015-02-26 16:19:40 -0700

    removed jslint/jshint errors and warnings

--- app.js
+++ app.js
@@ -1,50 +1,51 @@
-(function() {
-  var app = angular.module('store', [ ] );
+/*global angular */
+(function () {
+  'use strict';
+  var app = angular.module('store', [ ]),
+    gems = [
+      {
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
+        images: [
+          {
+            full: 'http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-02.gif',
+            thumb: ''
+          },
+          {
+            full: '',
+            thumb: ''
+          }
+        ]
+      },
+      {
+        name: 'Pentagonal Gem',
+        price: 5.95,
+        description: 'Origin of the Pentagonal Gem is unknown, hence its low value. It has a very high shine and 12 sides.',
+        images: [
+          {
+            full: 'http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-01.gif',
+            thumb: ''
+          },
+          {
+            full: '',
+            thumb: ''
+          }
+        ]
+      }
+    ];
   app.controller('StoreController', function () {
     this.products = gems;
   });
   app.controller('PanelController', function () {
     this.tab = 1;
 
-    this.selectTab = function(setTab) {
+    this.selectTab = function (setTab) {
       this.tab = setTab;
     };
 
-    this.isSelected = function(checkTab) {
+    this.isSelected = function (checkTab) {
       return this.tab === checkTab;
     };
   });
-
-  var gems = [
-  {
-    name: 'Dodecahedron',
-    price: 2.95,
-    description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
-    images: [
-      {
-        full: 'http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-02.gif',
-        thumb: '',
-      },
-      {
-        full: '',
-        thumb: '',
-      }
-    ],
-  },
-  {
-    name: 'Pentagonal Gem',
-    price: 5.95,
-    description: 'Origin of the Pentagonal Gem is unknown, hence its low value. It has a very high shine and 12 sides.',
-    images: [
-      {
-        full: 'http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-01.gif',
-        thumb: '',
-      },
-      {
-        full: '',
-        thumb: '',
-      }
-    ],
-  }
-];
-})();
+}());
