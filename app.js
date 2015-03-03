bdf9bcc9c84ff4ed92de6d688ba1a236bebbd140
Ken Wilcox <wilcoxk@gmail.com>
2015-03-03 00:28:41 -0700

    using product module

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*global angular */
 (function () {
   'use strict';
-  var app = angular.module('store', [ ]),
+  var app = angular.module('store', ['store-products']),
     gems = [
       {
         name: 'Dodecahedron',
@@ -57,36 +57,6 @@
     this.addReview = function (product) {
       product.reviews.push(this.review);
       this.review = {};
-    };
-  });
-  app.directive('productTitle', function () {
-    return {
-      restrict: 'E',
-      templateUrl: 'product-title.html'
-    };
-  });
-  app.directive('productDescription', function () {
-    return {
-      restrict: 'A',
-      templateUrl: 'product-description.html'
-    };
-  });
-  app.directive('productPanels', function () {
-    return {
-      restrict: 'E',
-      templateUrl: 'product-panels.html',
-      controller: function () {
-        this.tab = 1;
-
-        this.selectTab = function (setTab) {
-          this.tab = setTab;
-        };
-
-        this.isSelected = function (checkTab) {
-          return this.tab === checkTab;
-        }
-      },
-      controllerAs: 'panel'
     };
   });
 }());
