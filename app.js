5bfa65f51cd93c3e7d7bb5639e59d81e66356293
Ken Wilcox <wilcoxk@gmail.com>
2015-03-03 00:46:10 -0700

    using a service and Dependency Injection

--- app.js
+++ app.js
@@ -1,56 +1,14 @@
 /*global angular */
 (function () {
   'use strict';
-  var app = angular.module('store', ['store-products']),
-    gems = [
-      {
-        name: 'Dodecahedron',
-        price: 2.95,
-        description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
-        images: [
-          {
-            full: 'http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-02.gif',
-            thumb: ''
-          },
-          {
-            full: '',
-            thumb: ''
-          }
-        ],
-        reviews: [
-          {
-            stars: 5,
-            body: "I love this product!",
-            author: "joe@thomas.com"
-          },
-          {
-            stars: 1,
-            body: "This product sucks",
-            author: "tim@hater.com"
-          }
-        ]
-      },
-      {
-        name: 'Pentagonal Gem',
-        price: 5.95,
-        description: 'Origin of the Pentagonal Gem is unknown, hence its low value. It has a very high shine and 12 sides.',
-        images: [
-          {
-            full: 'http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-01.gif',
-            thumb: ''
-          },
-          {
-            full: '',
-            thumb: ''
-          }
-        ],
-        reviews: [
-        ]
-      }
-    ];
-  app.controller('StoreController', function () {
-    this.products = gems;
-  });
+  var app = angular.module('store', ['store-products']);
+  app.controller('StoreController', ['$http', function ($http) {
+    var store = this;
+    store.products = [ ];
+    $http.get('/products.json').success(function(data) {
+      store.products = data;
+    });
+  }]);
   app.controller('ReviewController', function () {
     this.reviews = {};
 
